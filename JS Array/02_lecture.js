ce66123b80eff66528ae954957a676d668372c9f
Ganesh Prasad <127683478+ganesh2394@users.noreply.github.com>
2024-12-18 18:31:18 +0530

    continue with 02_lecture from array creation

---
+++ JS Array/02_lecture.js
@@ -0,0 +1,30 @@
+// Array Creation
+
+// 1. Create using Literal:- 
+
+let a = [];     // create an empty array
+
+console.log(a);         // []
+
+// Creating an Array and Initializing with Values
+let b = [1, 2, 3];
+
+console.log(b);         // [ 1, 2, 3 ]
+
+// 2. Create using new Keyword (Constructor):-
+
+let aa = new Array();
+
+console.log(aa);        // []
+
+let bb = new Array(1,2,3);
+
+console.log(bb);        // [ 1, 2, 3 ]
+
+/** Note: Both the above methods do exactly the same. Use the array literal method for 
+ * efficiency, readability, and speed.
+ */
+
+
+
+
