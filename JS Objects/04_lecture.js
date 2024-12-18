b5aefb815f9d7aa91fec9153feae6d508a763f0b
Ganesh Prasad <127683478+ganesh2394@users.noreply.github.com>
2024-12-18 12:51:29 +0530

    concept of the json javascript in 04_lecture.js file

---
+++ JS Objects/04_lecture.js
@@ -0,0 +1,9 @@
+/**                 JavaScript JSON Objects
+ * 
+ * JSON (JavaScript Object Notation) is a lightweight data format used for exchanging data between 
+ * a server and a client or between different parts of an application. JSON is derived from 
+ * JavaScript but is language-independent, meaning it can be used in many programming languages.
+    In JavaScript, JSON objects are simply text formatted as key-value pairs, resembling 
+    JavaScript objects. However, there are some important distinctions between JSON and JavaScript objects.
+ * 
+ */
